c01f0c71281e3f17fec2264779175d2e9c0f0aee
Vasim <vahabov.vasim@gmail.com>
2025-08-31 01:37:41 +0200

    initial commit

---
+++ src/main/java/com/travelbookingsystem/edgeserver/EdgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.travelbookingsystem.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelbookingsystem/edgeserver/EdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbookingsystem.edgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
