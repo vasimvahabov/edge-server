013d0aa09502656aee3d7f324686664f139b7935
Vasim <vahabov.vasim@gmail.com>
2025-09-03 01:43:04 +0200

    feat : add sample integration test

--- src/test/java/com/travelbookingsystem/edgeserver/EdgeServerApplicationTests.java
+++ src/test/java/com/travelbookingsystem/edgeserver/EdgeServerApplicationTests.java
@@ -1,13 +1,38 @@
 package com.travelbookingsystem.edgeserver;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 class EdgeServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    static final int REDIS_PORT = 6379;
+
+    @Container
+    static GenericContainer<?> redisContainer =
+            new GenericContainer<>(DockerImageName.parse("redis:8.2.1"))
+                    .withExposedPorts(REDIS_PORT);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", ()-> redisContainer.getHost());
+        registry.add("spring.data.redis.port", ()-> redisContainer.getMappedPort(REDIS_PORT));
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
