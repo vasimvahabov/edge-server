c707e3f7b1c8a4e63b6977ba6c63a2c793cea195
Vasim <vahabov.vasim@gmail.com>
2025-09-14 21:52:06 +0200

    feat : enable http basic authentication

---
+++ src/main/java/com/travelbookingsystem/edgeserver/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.travelbookingsystem.edgeserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange ->
+                        exchange.anyExchange().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+}
