f7c4dc9b8ef7a9728fb16c2feca90c14b39496c9
Vasim <vahabov.vasim@gmail.com>
2025-10-14 01:01:57 +0200

    feat: make edge-server stateless by storing access tokens in Redis

--- src/main/java/com/travelbookingsystem/edgeserver/config/SecurityConfig.java
+++ src/main/java/com/travelbookingsystem/edgeserver/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
@@ -63,6 +65,11 @@ public class SecurityConfig {
                 exchange.<Mono<CsrfToken>>getAttributeOrDefault(CsrfToken.class.getName(), Mono.empty())
                         .doOnNext(token -> log.debug("CSRF Token generated: {}", token.getToken()))
                         .then(chain.filter(exchange));
+    }
+
+    @Bean
+    ServerOAuth2AuthorizedClientRepository authorizedClientRepository() {
+        return new WebSessionServerOAuth2AuthorizedClientRepository();
     }
 
     /*@Bean
